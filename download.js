d34196feac87abe9bd1b3f2b357d0a032dfec72d
wixboops <coke3@dr.com>
2024-12-02 21:53:10 -0600

    Update download.js

--- download.js
+++ download.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadButton').addEventListener('click', function() {
     // Replace 'your-file-path-here.exe' with the actual path to your downloadable file
-    const fileUrl = 'WixBootstrapper-v2.6.exe';
+    const fileUrl = 'v2.6.zip';
     
     // Create a temporary anchor element
     const link = document.createElement('a');
@@ -9,7 +9,7 @@ document.getElementById('downloadButton').addEventListener('click', function() {
     link.href = fileUrl;
     
     // Set the download attribute with a filename
-    link.download = 'WixBootstrapper-v2.6.exe';
+    link.download = 'v2.6.zip';
     
     // Append to the body
     document.body.appendChild(link);
